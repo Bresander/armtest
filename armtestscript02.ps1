fcc77d0bde1c7ef314fb335fa77a1c60c5df97c9
Bresander <119049271+Bresander@users.noreply.github.com>
2023-09-03 22:48:01 +0200

    Add files via upload

---
+++ armtestscript02.ps1
@@ -0,0 +1,10 @@
+﻿# Promote the server to a domain controller
+$DomainName = "bresander.se"
+$SafeModeAdministratorPassword = ConvertTo-SecureString -String "YourPassword" -AsPlainText -Force
+Install-ADDSForest -DomainName $DomainName -SafeModeAdministratorPassword $SafeModeAdministratorPassword
+
+# Reboot the server
+Restart-Computer -Force
+
+# Configuration complete
+Write-Host "Active Directory and DNS installation completed. The server will now reboot."
