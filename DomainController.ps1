6cf55fefd8c0d4031479098d26f6adc4a49e0dc9
Bresander <119049271+Bresander@users.noreply.github.com>
2023-09-07 00:35:11 +0200

    Update DomainController.ps1

--- DomainController.ps1
+++ DomainController.ps1
@@ -1,1 +1,27 @@
+param (
+    [Parameter(Mandatory=$true)][string]$domainName,
+    [Parameter(Mandatory=$true)][string]$adminUsername,
+    [Parameter(Mandatory=$true)][string]$AdminPassword
+)
 
+# Install Active Directory Domain Services
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+
+# Create a new domain in a new forest
+
+
+$Params = @{
+    DomainName = $domainName
+    DomainNetbiosName = $domainName.Split('.')[0]
+    SafeModeAdministratorPassword = (ConvertTo-SecureString -AsPlainText $AdminPassword -Force)
+    CreateDnsDelegation = $false
+    DatabasePath = 'C:\Windows\NTDS'
+    DomainMode = 'WinThreshold'
+    ForestMode = 'WinThreshold'
+    InstallDns = $true
+    LogPath = 'C:\Windows\NTDS'
+    NoRebootOnCompletion = $false
+    SysvolPath = 'C:\Windows\SYSVOL'
+    Force = $true
+    }
+Install-ADDSForest @Params
